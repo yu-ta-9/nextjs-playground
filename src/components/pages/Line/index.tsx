f530245660c6df7d574c2299285737c0af46aae9
Yuta Matsuo <y.matsuo.dev@gmail.com>
2023-08-07 17:22:27 +0900

    add firebase

--- src/components/pages/Line/index.tsx
+++ src/components/pages/Line/index.tsx
@@ -1,17 +1,42 @@
 'use client';
 
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+
+export const nonceGen = (length: number) => {
+  let result = '';
+
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  let charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
 
 export const Line: FC = () => {
+  const [nonce, setNonce] = useState('');
+  const [digestNonce, setDigestNonce] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      const crypto = require('crypto');
+      const nonce = nonceGen(32);
+      const digestNonce = await crypto.createHash('sha256').update(nonce).digest('hex');
+      setNonce(nonce);
+      setDigestNonce(digestNonce);
+    })();
+  }, []);
+
   const handleLogin = async () => {
     const clientId = process.env.NEXT_PUBLIC_LINE_CLIENT_ID;
     const redirectUri = 'http://localhost:3033/line/callback';
 
     const crypto = require('crypto');
-    const state = crypto.randomBytes(16).toString('base64');
-    const nonce = crypto.randomBytes(16).toString('base64');
+    const state = crypto.randomBytes(16).toString('hex');
 
-    window.location.href = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}&scope=profile%20openid&nonce=${nonce}`;
+    window.location.href = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}&scope=profile%20openid%20email`;
   };
 
   return (
@@ -20,6 +45,12 @@ export const Line: FC = () => {
       <button type='button' onClick={handleLogin}>
         Login
       </button>
+
+      <p>nonce:</p>
+      <p>{nonce}</p>
+
+      <p>digestNonce:</p>
+      <p>{digestNonce}</p>
     </div>
   );
 };
---
+++ src/lib/firebase/utils/auth.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { createContext, useEffect, useState, useContext } from 'react';
+
+import { getFirebaseApp } from '@/lib/firebase/utils/init';
+import { AuthContextState, ReactNodeProps } from '@/lib/firebase/type';
+import { User, getAuth, getRedirectResult } from 'firebase/auth';
+
+const FirebaseAuthContext = createContext<AuthContextState>({
+  currentUser: undefined,
+});
+
+// 認証プロバイダ
+const FirebaseAuthProvider = ({ children }: ReactNodeProps) => {
+  const [currentUser, setCurrentUser] = useState<User | null | undefined>(undefined);
+
+  const firebaseApp = getFirebaseApp();
+  const auth = getAuth(firebaseApp);
+
+  // authはnullの可能性があるので、useEffectの第二引数にauthを指定しておく
+  useEffect(() => {
+    /**
+     * authオブジェクトのログイン情報の初期化はonAuthStateChanged 発火時に行われる
+     * onAuthStateChangedが発火する前(authオブジェクトの初期化が完了する前)にcurrentUserを参照してしまうと、ログインしていてもnullになってしまう
+     * @see {@link https://firebase.google.com/docs/auth/web/manage-users}<br>
+     * そのため、userデータの参照はonAuthStateChanged内で行う
+     */
+    const unsubscribed = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setCurrentUser(user);
+      }
+      getRedirectResult(getAuth(firebaseApp));
+    });
+    return () => {
+      // onAuthStateChangedはfirebase.Unsubscribeを返すので、ComponentがUnmountされるタイミングでUnsubscribe(登録解除)しておく
+      unsubscribed();
+    };
+  }, [auth]);
+
+  return <FirebaseAuthContext.Provider value={{ currentUser: currentUser }}>{children}</FirebaseAuthContext.Provider>;
+};
+
+export { FirebaseAuthContext, FirebaseAuthProvider };
+
+export const userFirebaseAuthContext = () => useContext(FirebaseAuthContext);
